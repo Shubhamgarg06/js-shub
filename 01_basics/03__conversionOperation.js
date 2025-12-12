69cddbdab2caea5733b8b7c1c2d87dac7270403d
Shubham garg <85923764+Shubhamgarg06@users.noreply.github.com>
2025-12-12 18:03:50 +0000

    convrsion of datatypes

--- 01_basics/03__conversionOperation.js
+++ 01_basics/03__conversionOperation.js
@@ -1,4 +1,37 @@
-let score = 33
+// let score = 33
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score) //number 
+
+// console.log(typeof(score)) // number
+// let score = "33abc"
+
+// console.log(typeof score)  //string
+// console.log(typeof(score))  // string
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) 
+// console.log(valueInNumber) 
+
+
+// "33" ==> 33
+// "33abc" ==> NaN
+// true ==> 1, false =>0
+
+//let isLoggedIn= 1
+//let isLoggedIn= ""
+//let isLoggedIn= "shubham"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true, 0 => false
+//"" => false
+//"shubham" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
