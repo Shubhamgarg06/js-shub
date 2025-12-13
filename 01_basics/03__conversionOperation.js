8bdfb4f3b037221a1cba1c0a4bf63c3ca0e3b0a0
Shubham garg <85923764+Shubhamgarg06@users.noreply.github.com>
2025-12-13 12:30:12 +0000

    operations

--- 01_basics/03__conversionOperation.js
+++ 01_basics/03__conversionOperation.js
@@ -28,10 +28,32 @@
 //"" => false
 //"shubham" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 
+//************operations */
 
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/3)
+// console.log(2**2)
+// console.log(2%3)
+
+// let str1 = "shubham"
+// let str2 = "garg"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+console.log("1" +2)  //12
+console.log(1 + "2")  //12
+console.log("1" +2 + 2) //122
+console.log(1 + 2+ "2") //32
